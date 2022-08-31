9aa8292f3615a8bc45ec1060f817a22a60ad9d21
Ash Blue <ash@clevercrowgames.com>
2022-08-30 20:37:40 -0700

    ci(oyster): upgraded to latest version

--- build.js
+++ build.js
@@ -1,3 +1,3 @@
-const createDist = require('upm-package-populator');
+const { populatePackage } = require('upm-package-populator');
 
-createDist('Assets/com.fluid.database', '.', 'dist');
+populatePackage('.', 'Assets/com.fluid.database');
